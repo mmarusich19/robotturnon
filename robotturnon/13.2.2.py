a84fb61b0382591376351b2f6a80b1a7554b3c71
mmarusich19 <33101827+mmarusich19@users.noreply.github.com>
2018-06-04 15:13:47 -0500

    Update 13.2.2.py

--- robotturnon/13.2.2.py
+++ robotturnon/13.2.2.py
@@ -1,23 +1,20 @@
-import time
+import RoboPiLib as RPL
 import setup
-from setup import RPL
+import time
 import post_to_web as PTW # see post_to_web.py for instructions
 
 sensor_pin = 16
 RPL.pinMode(sensor_pin,RPL.INPUT)
 
-
 while True:
   PTW.state['d1'] = RPL.digitalRead(sensor_pin)
   PTW.post()
 
-  import RoboPiLib as RPL
-  import setup
   RPL.servoWrite(0,1000)
   RPL.servoWrite(1,2000)
   if RPL.digitalRead(sensor_pin) == 0:
      RPL.servoWrite(0,1000)
-     RPL.servoWrite(1,15000)
+     RPL.servoWrite(1,1500)
      time.sleep(2)
      RPL.servoWrite(0,1000)
      RPL.servoWrite(1,2000)
